977c0793f921218d0c842292f14c8541c9fc92a1
kowalks <guilhermekowalczuk@gmail.com>
2021-10-09 22:24:25 -0300

    deploy test 16

--- database/index.js
+++ database/index.js
@@ -1,22 +1,28 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
-const Student = require('../models/Student');
-const Class = require('../models/Class');
-const Event = require('../models/Event');
-const Absence = require('../models/Absence');
-const Question = require('../models/Question');
-const Exam = require('../models/Exam');
-const Message = require('../models/Message');
-const Admin = require('../models/Admin');
-const AdminToken = require('../models/AdminToken');
-const StudentToken = require('../models/StudentToken');
+const Student = require("../models/Student");
+const Class = require("../models/Class");
+const Event = require("../models/Event");
+const Absence = require("../models/Absence");
+const Question = require("../models/Question");
+const Exam = require("../models/Exam");
+const Message = require("../models/Message");
+const Admin = require("../models/Admin");
+const AdminToken = require("../models/AdminToken");
+const StudentToken = require("../models/StudentToken");
 
-const config = require('../config/config.json')
+const config = require("../config/config.json");
 
 if (process.env.DATABASE_URL) {
-    connection = new Sequelize(config.production)
+  connection = new Sequelize(process.env.DATABASE_URL, {
+    dialect: "postgres",
+    protocol: "postgres",
+    dialectOptions: {
+      ssl: true,
+    },
+  });
 } else {
-    connection = new Sequelize(config.development)
+  connection = new Sequelize(config.development);
 }
 
 // const { configure } = require('sequelize-pg-utilities')
@@ -25,7 +31,6 @@ if (process.env.DATABASE_URL) {
 // console.log(name, user, password, options)
 // const connection = new Sequelize(name, user, password, options);
 // console.log(connection)
-
 
 // const dbConfig = require('../config/database.js')
 // const connection = new Sequelize(dbConfig)
@@ -54,6 +59,5 @@ Class.associate(connection.models);
 Admin.associate(connection.models);
 AdminToken.associate(connection.models);
 StudentToken.associate(connection.models);
-
 
 module.exports = connection;
