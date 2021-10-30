cc28df440965d807f68fbed141f716fcac19a423
Guilherme Kowalczuk <guilhermekowalczuk@gmail.com>
2021-10-30 20:24:49 -0300

    added cors for web compatibility

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
-const express = require('express');
-const routes = require('./routes')
+const express = require("express");
+const routes = require("./routes");
+const cors = require("cors");
 
-require('./database');
+require("./database");
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
 
-app.listen(process.env.PORT || 3000,
-    () => console.log('Server is running...'));
+app.use((req, res, next)=>{
+    res.header('Access-Control-Allow-Origin','*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH');
+    app.use(cors());
+    next();
+})
+
+app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
