4a79c9f2f3245f234e2ddd4aa236d5629fb604b3
Guilherme Kowalczuk <guilhermekowalczuk@gmail.com>
2021-10-30 20:53:15 -0300

    test 1 web

--- server.js
+++ server.js
@@ -9,11 +9,13 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
-app.use((req, res, next)=>{
-    res.header('Access-Control-Allow-Origin','*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH');
-    app.use(cors());
-    next();
-})
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+//   app.use(cors());
+//   next();
+// });
+
+app.use(cors());
 
 app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
