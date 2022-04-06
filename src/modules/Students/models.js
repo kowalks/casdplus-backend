210411c7c1787c6898c2fda32543e9954dbeaa85
rafaelfrisch <rafaelcassolfrisch@gmail.com>
2022-04-06 13:05:54 -0300

    Refactor Absence

--- src/modules/Students/models.js
+++ src/modules/Students/models.js
@@ -121,7 +121,29 @@ class StudentToken extends Model {
   }
 }
 
+class Absence extends Model {
+  static init(connection) {
+     super.init(            {
+           date: DataTypes.DATEONLY,
+           justification: DataTypes.STRING,
+           file: DataTypes.STRING
+     }, {
+           sequelize: connection,
+           tableName: 'absence',
+           freezeTableName: true,
+     })
+  }
+}
+
+Absence.associate = function(models) {
+  Absence.belongsTo(models.Student, {
+   foreignKey: 'student_id',
+   target: 'id'
+  });
+}
+
 module.exports = { 
   Student,
   StudentToken, 
+  Absence,
 };
