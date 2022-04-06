1e5cc1d85ebd1e58e1a7f926d177ce14922b188d
rafaelfrisch <rafaelcassolfrisch@gmail.com>
2022-04-06 10:21:25 -0300

    Refactor event controller

---
+++ src/modules/Events/controllers.js
@@ -0,0 +1,49 @@
+const Class = require("../models/Class");
+const Event = require("../models/Event");
+const Admin = require("../models/Admin");
+const Student = require("../models/Student");
+
+module.exports = {
+  async store(req, res) {
+    [res, id] = await Admin.validate(req, res);
+    if (!id) return res;
+
+    const { name, description, date, class_id } = req.body;
+
+    if (!name || !description || !date)
+      return res.status(406).send("Please provide full information.");
+
+    const class_ = await Class.findByPk(class_id);
+
+    if (!class_) {
+      return res.status(406).json({ error: "Class not found" });
+    }
+
+    const event = await Event.create({ name, description, date, class_id });
+
+    return res.json(event);
+  },
+
+  async events(req, res) {
+    [res, id] = await Admin.validate(req, res);
+    if (!id) return res;
+
+    const events = await Event.findAll();
+
+    return res.status(200).json(events);
+  },
+
+  async destroy(req, res) {
+    [res, id] = await Admin.validate(req, res);
+    if (!id) return res;
+
+    var { event_id } = req.params;
+    const event = await Event.findByPk(event_id);
+
+    if (!event)
+      return res.status(404).send("Bad request: event_id not found.");
+
+    event.destroy();
+    return res.status(200).send("Deleted.");
+  },
+};
